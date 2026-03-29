a0fde63aab1465b202b393ca5e040e08672892aa
Sangmin1008 <sangmin11119@gmail.com>
2026-03-29 22:33:06 +0900

    feat : Hybrid ECS 기반 InputSystem 구현
    InputDataComponent를 이용하여 ECS World에 입력 정보를 직접 주입하는 방식

---
+++ Assets/02. Scripts/InputSystem/PlayerInputManager.cs
@@ -0,0 +1,69 @@
+using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public struct PlayerInputComponent : IComponentData
+{
+    public float2 Movement;
+    public bool IsMoving;
+}
+
+// GameObject World <=> ECS World 브릿지 역할 (Hybrid ECS)
+public class PlayerInputManager : MonoBehaviour
+{
+    private EntityManager _entityManager;
+    private Entity _playerInputEntity;
+    
+    // InputSystem Action 참조
+    private InputSystem_Actions _inputSystem;
+    private InputAction _moveAction;
+
+    private void Awake()
+    {
+        _inputSystem = new InputSystem_Actions();
+        _moveAction = _inputSystem.Player.Move;
+        _moveAction.Enable();
+    }
+
+    private void Start()
+    {
+        // ECS World의 EntityManager
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        
+        // 싱글톤 Entity 생성
+        // _playerInputEntity = _entityManager.CreateEntity();
+        
+        // 컴포넌트 추가
+        // _entityManager.AddComponentData(_playerInputEntity, new PlayerInputComponent());
+        
+        _playerInputEntity = _entityManager.CreateEntity(typeof(PlayerInputComponent));
+    }
+
+    private void OnEnable()
+    {
+        _moveAction.performed += OnMove;
+        _moveAction.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.performed -= OnMove;
+        _moveAction.canceled -= OnMove;
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        float h = ctx.ReadValue<Vector2>().x;
+        float v = ctx.ReadValue<Vector2>().y;
+        
+        _entityManager.SetComponentData(_playerInputEntity, new PlayerInputComponent
+        {
+            Movement = new float2(h, v),
+            IsMoving = ctx.phase == InputActionPhase.Performed
+        });
+    }
+}
